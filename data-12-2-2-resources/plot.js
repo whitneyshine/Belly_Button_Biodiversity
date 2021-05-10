c7664dd66bd48de65ad61038e97639caaa3b2bb3
whitneyshine <wshine79@sbcglobal.net>
2021-05-10 10:31:06 -0500

    spaceX

---
+++ data-12-2-2-resources/plot.js
@@ -0,0 +1,13 @@
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: { title: "City" },
+    yaxis: { title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
+
